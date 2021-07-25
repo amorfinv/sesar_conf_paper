73f7b2d7759ae74828911f96f2241ac1096e2c06
Andres Morfin Veytia <78442543+amorfinv@users.noreply.github.com>
2021-07-25 16:40:34 +0200

    update

--- graph_definition/group_split.py
+++ graph_definition/group_split.py
@@ -4,6 +4,7 @@ from os import path
 import graph_funcs
 
 def main():
+    # gis data pah
     gis_data_path = 'gis'
 
     # load cleaned graph
@@ -12,10 +13,12 @@ def main():
     # convert to gdf
     nodes, edges = ox.graph_to_gdfs(G)
 
-    # split groups
+    # split groups at degree-2 nodes with 90 degree
     nodes, edges= graph_funcs.new_groups_90(nodes, edges)
     
+    # convert back to graph and save
     G = ox.graph_from_gdfs(nodes, edges)
+    ox.save_graphml(G, filepath=path.join(gis_data_path, 'streets', 'split_groups.graphml'))
 
     ox.save_graph_geopackage(G, filepath=path.join(gis_data_path, 'streets', 'split_group.gpkg'))
 
